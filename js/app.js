27493977e56790f69164982f80f9ba1c452b584b
dediwibisono17 <dedi.wibisono17@gmail.com>
2025-06-23 08:49:55 +0700

    ded

--- js/app.js
+++ js/app.js
@@ -122,3 +122,26 @@ function ScrollIndicator() {
 }
 
 window.onscroll = function () { ScrollIndicator() };
+
+
+
+gsap.timeline({
+    scrollTrigger: {
+        trigger: ".box",
+        start: "center center",
+        end: "bottom top",
+        scrub: 1,
+        pin: true
+    }
+})
+    .from(".text1", {
+        x: innerWidth * 1
+    })
+    .from(".text2", {
+        x: innerWidth * -1
+    })
+    .from(".angkax", {
+        y: innerHeight * 1,
+        rotate: 360
+    })
+
